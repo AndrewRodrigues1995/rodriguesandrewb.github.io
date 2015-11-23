10fe6772159c8f198d6a39fc1fe4354e9ff501aa
Andrew Rodrigues <rodriguesandrewb@gmail.com>
2015-11-22 21:43:10 -0800

    Change preload to dom directly

--- app.js
+++ app.js
@@ -11,26 +11,26 @@ var galleryImages = [
 	{image: "url('4.jpg')",title: "Template & CSS Included",color: '#7f8c8d'},
 ];
 
-var i = 1;
-preload = ['1.jpg','2.jpg','3.jpg','4.jpg'];
+// var i = 1;
+// preload = ['1.jpg','2.jpg','3.jpg','4.jpg'];
 
-$(document).ready(function() {
-	for (var j = 0; j < preload.length; j++) {
-		$('#preload').append('<img src="' + preload[j] + '">');
-	}
-});
+// $(document).ready(function() {
+// 	for (var j = 0; j < preload.length; j++) {
+// 		$('#preload').append('<img src="' + preload[j] + '">');
+// 	}
+// });
 
-var change = function changeImage() {
-	if (i >= galleryImages.length) {
-		i = 0;
-	};
-	var nextImage = galleryImages[i].image;
-	$('#fullGallery').css('background-image', galleryImages[i].image);
-	$('#title').css('color', galleryImages[i].color);
-	$('#button').css('background', galleryImages[i].color);
-	$('#title').text(galleryImages[i].title);
-	i = i + 1;
-}
+// var change = function changeImage() {
+// 	if (i >= galleryImages.length) {
+// 		i = 0;
+// 	};
+// 	var nextImage = galleryImages[i].image;
+// 	$('#fullGallery').css('background-image', galleryImages[i].image);
+// 	$('#title').css('color', galleryImages[i].color);
+// 	$('#button').css('background', galleryImages[i].color);
+// 	$('#title').text(galleryImages[i].title);
+// 	i = i + 1;
+// }
 
 $('#fullGallery').css('background-image', galleryImages[0].image);
 $('#title').text(galleryImages[0].title);
